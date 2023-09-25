ec7230a93e70ff3a90e4d055770923a5f3c86bdd
Aliaxandr Lapitski <lepitale@gmail.com>
2023-09-25 16:06:30 +0300

    Условия базовой задачи выполнено

--- PracticumButton/ViewController.swift
+++ PracticumButton/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var labelValue: UILabel!
     
+    var count: Int = 0
+    
     
     @IBOutlet weak var butonValue: UIButton!
     
@@ -18,9 +20,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.black
+        labelValue.textColor = UIColor.white
+        
         // Do any additional setup after loading the view.
+        labelValue.font = UIFont.systemFont(ofSize: 30)
+        labelValue.text = "Значение счетчика: \(count)"
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        count += 1
+        labelValue.text = "Значение счетчика: \(count)"
+        
+//        print("Нажатие")
+    }
+    
 }
 
